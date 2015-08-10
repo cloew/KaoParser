e9668c9dc654c921c51d4472f50fab55519fd9c1
Chris Loew <cloew123@gmail.com>
2015-08-09 18:52:21 -0600

    Can now compile grammars that are dependent on other grammars

--- kao_parser/grammar.py
+++ kao_parser/grammar.py
@@ -2,10 +2,32 @@ from enum import Enum
 from kao_decorators import lazy_property
 import re
 
+dependencyRegEx = re.compile("<[a-zA-Z]+>")
+
 class Grammar(Enum):
     """ Represents the Grammar to use when parsing text """
         
     @lazy_property
+    def regexString(self):
+        """ Return the proper regex string """
+        regexString = self.value
+        for dependency in self.dependencies:
+            dependencySection = "<{0}>".format(dependency)
+            dependencyRegEx = "({0})".format(self.sibling(dependency).regexString)
+            regexString = regexString.replace(dependencySection, dependencyRegEx)
+        return regexString
+        
+    @lazy_property
     def regex(self):
         """ Compile and return this regex """
-        return re.compile(self.value)
+        return re.compile(self.regexString)
+        
+    @lazy_property
+    def dependencies(self):
+        """ Return this rule's dependencies """
+        results = dependencyRegEx.findall(self.value)
+        return [result[1:-1] for result in results]
+        
+    def sibling(self, name):
+        """ Return the sibling with the requested name """
+        return self.__class__.__members__[name]
